6e73e3cb48cc8587886ad04e9d12cbe55f376710
Serdar <serdar2225@gmail.com>
2020-08-18 21:22:42 -0400

    base commit

--- src/test/java/base.java
+++ src/test/java/base.java
@@ -1,13 +1,25 @@
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.remote.MobileCapabilityType;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class base {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
+
+        File f = new File("src");
+        File fs = new File(f,"ApiDemos-debug.apk");
 
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 emulator");
-        cap.setCapability(MobileCapabilityType.APP,"");
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 Emulator");
+        cap.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+
 
     }
 }
