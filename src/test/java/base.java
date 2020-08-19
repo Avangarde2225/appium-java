d1e7e742973e82af3680a32b85547b26541ab944
Serdar <serdar2225@gmail.com>
2020-08-18 22:36:54 -0400

    extending the base class

--- src/test/java/base.java
+++ src/test/java/base.java
@@ -1,5 +1,7 @@
 
 import io.appium.java_client.android.AndroidDriver;
+
+
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -9,7 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class base {
-    public static void main(String[] args) throws MalformedURLException {
+    public static AndroidDriver<AndroidElement> Capabilities () throws MalformedURLException {
 
         File f = new File("src");
         File fs = new File(f,"ApiDemos-debug.apk");
@@ -19,7 +21,7 @@ public class base {
         cap.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-
+        return driver;
 
     }
 }
