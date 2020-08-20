01a15e25996fe9b76edf21824d9be56be2a31a01
Serdar <serdar2225@gmail.com>
2020-08-20 16:41:20 -0400

    verification

--- src/test/java/gestures.java
+++ src/test/java/gestures.java
@@ -4,8 +4,11 @@ import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
 
 
 public class gestures extends base {
@@ -18,6 +21,16 @@ public class gestures extends base {
 
         WebElement expandList = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
         t.tap(tapOptions().withElement(element(expandList))).perform();
+
+        driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']").click();
+
+        WebElement pm = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+        t.longPress(longPressOptions().withElement(element(pm)).withDuration(ofSeconds(2))).release().perform();
+
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
+
+
+
 
     }
 }
