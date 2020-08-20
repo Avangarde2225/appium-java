464a7c622c6c447098f73563b7eb7a1c3ba29d67
Serdar <serdar2225@gmail.com>
2020-08-20 09:04:17 -0400

    ui automator

---
+++ src/test/java/UiAutomatorTest.java
@@ -0,0 +1,14 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class UiAutomatorTest extends base {
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElementByAndroidUIAutomator("(attribute=value");
+    }
+}
