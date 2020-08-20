1faee78047f381300cc397db7e49690f7c2b8764
Serdar <serdar2225@gmail.com>
2020-08-20 08:55:19 -0400

    first e2e test

--- src/test/java/basics.java
+++ src/test/java/basics.java
@@ -9,5 +9,11 @@ public class basics extends base{
         AndroidDriver<AndroidElement> driver = Capabilities();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+        driver.findElementByClassName("android.widget.EditText").sendKeys("helloDear");
+        driver.findElementsByClassName("android.widget.Button").get(1).click();
+
     }
 }
